bab5e7b21b3721710c98d796a181cb1dd623f761
remyavas <remyavas@yahoo.co.in>
2016-11-06 07:01:38 +0530

    hello first commit

---
+++ gridex/src/gridex/gridpull.java
@@ -0,0 +1,12 @@
+package gridex;
+
+import org.testng.annotations.Test;
+
+public class gridpull {
+	@Test
+	public void pull()
+	{
+		System.out.print("Pull");
+	}
+
+}
---
+++ gridex/src/gridex/gridpush.java
@@ -0,0 +1,13 @@
+package gridex;
+
+import org.testng.annotations.Test;
+
+public class gridpush {
+	
+	@Test
+	public void hello()
+	{
+		System.out.println("hello");
+	}
+
+}
